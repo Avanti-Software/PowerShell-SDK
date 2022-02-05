89ad37b5065f5159f04be0a9c4405d28e3c2199b
Adam Stirtan <adamstirtan@gmail.com>
2022-02-05 00:09:18 -0500

    chore: adds comments

--- Avanti.PowerShellSDK/State/AuthenticationState.cs
+++ Avanti.PowerShellSDK/State/AuthenticationState.cs
@@ -2,12 +2,24 @@
 
 namespace Avanti.PowerShellSDK.State
 {
+    /// <summary>
+    /// Represents the persisted authentication for Avanti Cmdlets in a pipeline.
+    /// </summary>
     public class AuthenticationState
     {
+        /// <summary>
+        /// The default or custom base URL.
+        /// </summary>
         public string BaseUrl { get; set; }
 
-        public DateTimeOffset ExpiresAt { get; set; }
-
+        /// <summary>
+        /// The application access token.
+        /// </summary>
         public string Token { get; set; }
+
+        /// <summary>
+        /// The point in time the token expires.
+        /// </summary>
+        public DateTimeOffset ExpiresAt { get; set; }
     }
 }
